ba6e95b7544b70b02684b29752ac27fabd473e68
Kekschorstviy <117016566+Kekschorstviy@users.noreply.github.com>
2024-11-13 12:52:33 +0100

    refactor: Change of User structure

--- backend/src/main/java/de/thu/thutorium/model/User.java
+++ backend/src/main/java/de/thu/thutorium/model/User.java
@@ -1,25 +1,36 @@
 package de.thu.thutorium.model;
 
 import jakarta.persistence.*;
-import java.util.UUID;
+
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "user_account")
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
   @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID id;
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "user_id")
+  private Long user_id;
 
+  @Column(name = "first_name", nullable = false)
   private String first_name;
+
+  @Column(name = "last_name", nullable = false)
   private String last_name;
+
   @Enumerated(EnumType.STRING)
   private UserRole role;
+
+  @Column(name = "is_verified", nullable = false)
   private Boolean is_verified = false;
 
-  // ...
+  @Column(name = "created_at", nullable = false)
+  private LocalDateTime createdAt;
 
 }
