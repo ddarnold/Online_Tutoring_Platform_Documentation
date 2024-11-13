ae93a41b81fd676f6de5bbe0edd427e620aa76cf
Dorotej-Dado Arnold <dorotej.d.arnold@gmail.com>
2024-11-13 16:51:58 +0100

    Added javaDoc

--- backend/src/main/java/de/thu/thutorium/repository/CourseRepository.java
+++ backend/src/main/java/de/thu/thutorium/repository/CourseRepository.java
@@ -3,4 +3,16 @@ package de.thu.thutorium.repository;
 import de.thu.thutorium.model.Course;
 import org.springframework.data.repository.CrudRepository;
 
+/**
+ * Repository interface for managing {@link Course} entities.
+ *
+ * <p>This interface extends the {@link CrudRepository} interface provided by Spring Data, enabling
+ * basic CRUD (Create, Read, Update, Delete) operations on Course entities.
+ *
+ * <p>By extending {@link CrudRepository}, this interface inherits several methods for working with
+ * Course persistence, including methods for saving, deleting, and finding Course entities.
+ *
+ * @see Course
+ * @see CrudRepository
+ */
 public interface CourseRepository extends CrudRepository<Course, Integer> {}
